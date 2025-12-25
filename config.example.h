82125ed35dfcae21942db69040dd62901343f525
tobiashahner-cmyk <tobiashahner@gmail.com>
2025-12-25 19:56:50 +0100

    Add files via upload

---
+++ config.example.h
@@ -0,0 +1,31 @@
+#pragma once
+
+// these are exactly MY devices in MY home theater setup.
+// to use your own devices, you need to set the constants here
+// AND alter them in the main sketch.
+// As I myself do obviously NOT do that, that may need a complete
+// rebuild of the hole SerialCommandDispatcher, KinoAPI,...
+
+// WLAN
+#define WIFI_SSID     "your_wifi_ssid"
+#define WIFI_PASSWORD "your_wifi_passwd"
+
+// Hue
+#define HUE_BRIDGE_IP "xxx.xxx.xxx.xxx"
+#define HUE_TOKEN     "your_hue_bridge_token"
+
+// Yamaha
+#define YAMAHA_IP     "xxx.xxx.xxx.xxx"
+
+// Optoma
+#define BEAMER_IP     "xxx.xxx.xxx.xxx"
+#define BEAMER_ID     1
+
+// Leinwand
+#define CANVAS_IP     "xxx.xxx.xxx.xxx"
+
+// Sound WLED
+#define SOUND_IP      "xxx.xxx.xxx.xxx"
+
+// Hyperion
+#define HYPERION_IP   "xxx.xxx.xxx.xxx"
