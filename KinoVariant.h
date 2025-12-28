7f0760137008b6fafc261244a4dec209a41b66f5
tobiashahner-cmyk <tobiashahner@gmail.com>
2025-12-28 20:01:38 +0100

    Add files via upload
    Fixed lifetime bug in KinoVariant::STRING

--- KinoVariant.h
+++ KinoVariant.h
@@ -1,7 +1,121 @@
 #pragma once
 #include <Arduino.h>   // <-- WICHTIG
 
-class KinoVariant {
+
+struct KinoVariant {
+    enum Type : uint8_t {
+        NONE,
+        INT,
+        BOOL,
+        FLOAT,
+        STRING
+    };
+
+    Type type = NONE;
+
+    union {
+        int32_t i;
+        bool b;
+        float f;
+    };
+
+    char* s = nullptr;
+
+    KinoVariant() = default;
+
+    ~KinoVariant() {
+        if (type == STRING && s) {
+            free(s);
+        }
+    }
+
+    KinoVariant(const KinoVariant& other) {
+        *this = other;
+    }
+
+    KinoVariant& operator=(const KinoVariant& other) {
+        if (this == &other) return *this;
+
+        if (type == STRING && s) {
+            free(s);
+        }
+
+        type = other.type;
+
+        switch (type) {
+            case STRING:
+                if (other.s) {
+                    s = strdup(other.s);
+                }
+                break;
+            case INT:   i = other.i; break;
+            case BOOL:  b = other.b; break;
+            case FLOAT: f = other.f; break;
+            default: break;
+        }
+        return *this;
+    }
+
+    static KinoVariant fromString(const char* v) {
+        KinoVariant k;
+        k.type = STRING;
+        k.s = v ? strdup(v) : nullptr;
+        return k;
+    }
+
+    static KinoVariant fromInt(int32_t v) {
+        KinoVariant k;
+        k.type = INT;
+        k.i = v;
+        return k;
+    }
+
+    static KinoVariant fromBool(bool v) {
+        KinoVariant k;
+        k.type = BOOL;
+        k.b = v;
+        return k;
+    }
+
+    static KinoVariant fromFloat(float v) {
+        KinoVariant k;
+        k.type = FLOAT;
+        k.f = v;
+        return k;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*class KinoVariant {
 public:
     enum Type : uint8_t {
         NONE,
@@ -50,3 +164,4 @@ public:
         return k;
     }
 };
+*/
